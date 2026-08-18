456f62e8e6cb0e18e3b85a51438c664ac4ac93db
Yash BIsht <yashbisht044@gmail.com>
2026-08-18 21:24:19 +0530

    Time: 5 ms (25.51%), Space: 9.7 MB (76.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()){
+        if(s.length() != t.length()){
             return false;
         }
-        unordered_map<char,int>m;
-
-        for (char c : s){
-            m[c]++;
-
-        }
-        for (auto c2 : t){
-            if (m.find(c2)==m.end()){
-                return false;
-            }
-            else {
-                m[c2]--;
-            }
-        }
-        for(auto ele:m){
-            if (ele.second != 0){
-                return false;
-            }
-            
-        }return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
         
     }
 };
