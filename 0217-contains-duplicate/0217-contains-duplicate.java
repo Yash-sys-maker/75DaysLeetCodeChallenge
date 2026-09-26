5b3e7ff45e3f3c52c7f88cbb14f370b9645051a4
Yash BIsht <yashbisht044@gmail.com>
2026-09-26 13:06:54 +0530

    Time: 24 ms (28.22%), Space: 81.4 MB (93.52%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for ( int y = 0 ; y < nums.length-1 ; y++){
-            if (nums[y] == nums[y+1]){
-                return true;
+        for ( int z = 0 ; z < nums.length-1 ; z++){
+            if (nums[z] == nums[z+1]){
+                return true ;
             }
         }
         return false;
